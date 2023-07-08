0bed4be717fec580fbe00c3866c39bb17582453d
clshu <clshu2020@gmail.com>
2023-07-08 01:42:39 -0700

    Add a test case. Create 2 shell scripts to run dev and test

---
+++ tests/quick_dev.rs
@@ -0,0 +1,12 @@
+#![allow(unused)]
+use anyhow::Result;
+use httpc_test::Error;
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:3000")?;
+
+    hc.do_get("/hello").await?.print().await?;
+
+    Ok(())
+}
