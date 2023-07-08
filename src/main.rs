a6ff2e30a1a339922752ac2b16f389bb344a53e8
clshu <clshu2020@gmail.com>
2023-07-08 00:41:16 -0700

    Hello World

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused)]
+use std::net::SocketAddr;
+
+use axum::response::Html;
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("<h1>Hello, <strong>World<strong>!<h1>") }),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // run it with hyper on localhost:3000
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
